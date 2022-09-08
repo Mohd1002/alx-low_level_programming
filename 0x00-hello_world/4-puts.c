8576f00f67b68452b9fa6fed0f60c7cbc31374a8
Mohd1002 <animefreak1002@gmail.com>
2022-09-08 23:52:21 +0300

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+/**
+ * main - Entry point it prints a text
+ * Return: Always 0 (Success)
+ */
 int main(void)
+
 {
 	char answer[100] = "\"Programming is like building a multilingual puzzle";
 
