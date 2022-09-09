67a796939de0da0f00c5ebcb3e47a4aface5e864
Mohd1002 <animefreak1002@gmail.com>
2022-09-10 00:33:20 +0300

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+        char x,lowercase;
+
+        for (x = 'Z'; x >= 'A'; x--)
+        {
+        lowercase = tolower(x);
+        putchar(lowercase);
+        }
+        putchar('\n');
+return (0);
+}
