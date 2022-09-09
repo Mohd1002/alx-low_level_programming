57648e675e42a080b9c20b8e78092dd6fb1dbcf9
Mohd1002 <animefreak1002@gmail.com>
2022-09-09 22:47:34 +0300

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int x, lowercase;
+
+	for (x = 'A'; x <= 'Z';x++)
+	{
+	if (x == 'E')
+	{
+		continue;
+	}
+	else if (x == 'Q')
+	{
+		continue;
+	}
+	else
+	lowercase = tolower(x);
+	putchar(lowercase);
+	}
+	putchar('\n');
+return (0);
+}
