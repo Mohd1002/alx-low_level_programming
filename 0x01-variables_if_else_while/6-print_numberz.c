0946a35f9113f157030d60a54fb946373849982f
Mohd1002 <animefreak1002@gmail.com>
2022-09-10 00:15:07 +0300

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int set;
+
+	set = '0';
+	for (set = '0'; set <= '9'; set++)
+	putchar(set);
+	putchar('\n');
+return (0);
+}
