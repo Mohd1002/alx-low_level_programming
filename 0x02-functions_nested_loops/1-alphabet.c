bef0a6a67003d3f8949b88f142c5049482a0d672
Mohd1002 <animefreak1002@gmail.com>
2022-09-18 13:13:01 +0300

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * print_alphabet - prints the alphabet
- *@c: the character to print
- *
- * Return: On succes 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char c);
-
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char a = 'a';
 
@@ -19,5 +12,4 @@ int print_alphabet(void)
 		_putchar(a);
 	}
 	_putchar('\n');
-return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,6 @@
-#include <stdio.h>
-/* print_alphabet - prints the alphabet
- */
+#ifndef main_h
+#define main_h
+int _putchar(char c);
 
-int _putchar(char);
-
-int print_alphabet(void)
-{
-        char a = 'a';
-        for (a = 'a'; a <= 'z';a++)
-        {
-                _putchar(a);
-        }
-        _putchar('\n');
-return (0);
-}
+void print_alphabet(void);
+#endif
