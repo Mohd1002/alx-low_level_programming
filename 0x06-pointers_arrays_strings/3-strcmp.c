3f18bbfdb0ef3746710fde20c0e06071d3b717b0
Mohd1002 <animefreak1002@gmail.com>
2022-09-21 22:47:02 +0300

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: returns the value of _strcmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
