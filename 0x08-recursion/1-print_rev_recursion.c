f16c8fcb0aa768b7de48a4cb68850cb79b446c21
Mohd1002 <animefreak1002@gmail.com>
2022-09-29 16:55:27 +0300

    update

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints the string in reverse
+ * @s: the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (strlen(s) <= 0)
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(s[0]);
+}
