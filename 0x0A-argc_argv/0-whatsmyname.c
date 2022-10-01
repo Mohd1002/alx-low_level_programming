98bfdb35a062326bc475d8360ae91fbaaa018962
Mohd1002 <animefreak1002@gmail.com>
2022-10-01 19:44:14 +0300

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: returns 0 in success
+ * @argc: the number of arguments entered on the commandline
+ * @argv: the array of arg on the commandline
+ */
+int main(int argc, char *argv[])
+{
+	puts(argv[argc - argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Argc and v in C
