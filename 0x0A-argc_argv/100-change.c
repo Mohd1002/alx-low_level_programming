21c99771d87ebaa8f31e47b5b249b928bca88ae9
Mohd1002 <animefreak1002@gmail.com>
2022-10-02 21:03:01 +0300

    update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ */
+int main(int argc, char *argv[])
+{
+	int c = 25, ch = 10, cha = 5 , chan = 2 , chang = 1;
+	if (argc != 1)
+	{
+		printf("Error\n")
+		return (1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("0\n"
+		return (0);
+	}
+	
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: returns 0 if success
+ * @argc: no of args
+ * @argv: array of args
+ */
+int main(int argc, char *argv[])
+{
+        int co = 1, ans = 0;
+
+	if (argc == 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+        while (co < argc)
+        {
+		if (isdigit(*argv[co]))
+		{
+			ans += atoi(argv[co]);
+			co++;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", ans);
+	return (0);
+}
