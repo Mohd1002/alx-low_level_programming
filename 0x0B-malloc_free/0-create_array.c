f9e9b76432e7d79d956ca5b3a82655afcfd2d2c3
Mohd1002 <animefreak1002@gmail.com>
2022-10-05 05:08:49 +0300

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - creates an array of chars and initializes c
+ * @c: the char
+ * @size: the size of the array
+ * Return: returns the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *A = malloc(size * sizeof(c));
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	while (i <= size)
+	{
+		A[i] = c;
+		i++;
+	}
+	if (A == NULL)
+	{
+		return (A);
+	}
+	return (A);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+a project on malloc and free
