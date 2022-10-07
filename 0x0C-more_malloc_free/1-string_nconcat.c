7496b14fbe6dc49e4525a327327a7fc23d1a3a27
Mohd1002 <animefreak1002@gmail.com>
2022-10-07 03:53:46 +0300

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *strr;
-	unsigned long int chsum, i = 0, save = strlen(s1), s2len = strlen(s2) - n;
+	unsigned long int chsum, i = 0, save, s2len;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	save = strlen(s1);
 	if (n >= strlen(s2))
 	{
 		chsum = sizeof(char) * strlen(s1) + strlen(s2) + 1;
 	}
 	else
 	{
+		s2len = strlen(s2) - n;
 		chsum = sizeof(char) * strlen(s1) + strlen(s2) - s2len + 1;
 	}
 	strr = malloc(chsum);
-	while (i < strlen(s1))
+	if (strr == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(s1); i++)
 	{
 		strr[i] = s1[i];
-		i++;
 	}
 	if (n >= strlen(s2))
 	{
-		for (i = 0; i < strlen(s1); i++)
+		for (i = 0; i < strlen(s2); i++)
 		{
-			strr[i] = s1[i];
+			strr[save] = s2[i];
+			save++;
 		}
 	}
 	else
 	{
-		for (i = 0; i < strlen(s2) - s2len; i++)
+		for (i = 0; i < strlen(s2) - s2len; i++, save++)
 		{
 			strr[save] = s2[i];
-			save++;
 		}
 	}
 	return (strr);
