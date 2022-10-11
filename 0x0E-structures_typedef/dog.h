2c6fdc27b3356167158e6be514697cdebdfb77b1
Mohd1002 <animefreak1002@gmail.com>
2022-10-12 01:38:40 +0300

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef struct dog
+/**
+ * struct dog - the stats of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
