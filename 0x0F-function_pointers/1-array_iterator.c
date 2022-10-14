f4bdc5ad0c13a6bb7879972632f70e4e04c6e7e3
Mohd1002 <animefreak1002@gmail.com>
2022-10-14 05:44:01 +0300

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+/**
+ * array_iterator - iterates an array's elements
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function using the array's elements
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
 	for (i = 0; i < size; i++)
-	{	
+	{
 		action(array[i]);
 	}
 }
